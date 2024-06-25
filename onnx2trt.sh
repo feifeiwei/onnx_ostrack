1ea978376acf6edbd10df9f7f949c7c71766c167
feifeiwei <40533960+feifeiwei@users.noreply.github.com>
2024-06-25 09:25:22 +0800

    Update onnx2trt.sh

--- onnx2trt.sh
+++ onnx2trt.sh
@@ -1,1 +1,1 @@
-trtexec --onnx=sim_os_track.onnx --saveEngine=sim_os_track_fp32.engine  --verbose  --inputShape=x:1x3x256x256 --inputShape=z:1x3x128x128 --workspace=1400
+trtexec --onnx=sim_os_track.onnx --saveEngine=sim_os_track_fp32.engine  --verbose  --workspace=1400
